378e1a93d25d6609d94122508274e7dbbbf79f59
Mirza Firoz Uddin <mz.firozuddin@gmail.com>
2025-03-01 18:07:05 +0530

    add user schema

---
+++ src/models/userModel.js
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            trim: true,
+            unique: true,
+        },
+        phone: {
+            type: String,
+            required: true,
+            trim: true,
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+        address: {
+            type: String,
+            required: true,
+        },
+        role: {
+            type: String,
+            required: true,
+            enum: ["admin", "manager", "customer"],
+        },
+        isVerified: {
+            type: Boolean,
+            default: false,
+        },
+    },
+    { timestamps: true },
+);
+
+export const User = mongoose.model("User", userSchema);
