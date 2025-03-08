bc2a56afad5fcf81fbf90b5336ba0a12203fa37a
Mirza Firoz Uddin <mz.firozuddin@gmail.com>
2025-03-08 23:03:39 +0530

    add auth middleware

---
+++ src/middlewares/authMiddleware.js
@@ -0,0 +1,36 @@
+import jwt from "jsonwebtoken";
+import logger from "../config/logger";
+import { config } from "../config/config";
+import { authService } from "../services/authService";
+
+export const verifyJWT = async (req, res, next) => {
+    try {
+        //: Collect token
+        const token =
+            req.cookies?.accessToken ||
+            req.header("Authorization")?.replace("Bearer ", "");
+
+        if (!token) {
+            const error = (401, "Unauthorized Access!");
+            return next(error);
+        }
+
+        //: verify token
+        const decodedToken = jwt.verify(token, config.accessTokenSecret);
+
+        //: Find user
+        const user = await authService.getUserById(decodedToken?.sub);
+        if (!user) {
+            const error = (401, "Invalid access token!");
+            return next(error);
+        }
+
+        //: Now add user in req object
+        req.user = user;
+        next();
+    } catch (err) {
+        logger.error("ERROR: verifyJWT", { err });
+        next(err);
+        return;
+    }
+};
--- src/services/authService.js
+++ src/services/authService.js
@@ -8,6 +8,10 @@ class AuthService {
     async getUserByEmail(email) {
         return await User.findOne({ email });
     }
+
+    async getUserById(id) {
+        return await User.findById(id).select("-password -refreshToken -__v");
+    }
 }
 
 export const authService = new AuthService();
