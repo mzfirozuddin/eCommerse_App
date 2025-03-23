348b8b32fea055c2e8ac9322559ce1fab8db702f
Mirza Firoz Uddin <mz.firozuddin@gmail.com>
2025-03-23 23:49:31 +0530

    Add cors middleware

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 import express from "express";
+import cors from "cors";
 import cookieParser from "cookie-parser";
 import { globalErrorHandler } from "./middlewares/globalErrorHandler.js";
 import authRouter from "./routes/authRoute.js";
 
 const app = express();
+//: Middleware to handle CORS error
+app.use(cors({ origin: ["http://localhost:5173"], credentials: true }));
 //: Middleware for handle json data
 app.use(express.json());
 //: Middleware for handle data from url
